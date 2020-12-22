0cb43118ed68051c4ca8b01909f07a03b542518d
master <sungwoony@kakao.com>
2020-12-23 02:14:33 +0900

    From : create PostsApiController for  posts registration function (p.104) To : test findById method for posts view function (p.118) Next : automate post registration time and update time with JPA Auditing

--- src/main/java/io/web/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/io/web/book/springboot/domain/posts/PostsRepository.java
@@ -2,5 +2,6 @@ package io.web.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PostsRepository extends JpaRepository<Posts,Long> {
 }
