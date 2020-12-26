b5650f04a47c2f4ed0e9575756038fbb6d3f0a99
master <sungwoony@kakao.com>
2020-12-27 01:19:33 +0900

    From : 세션값 가져오는 부분 어노테이션 기반으로 리팩토링 (p.195) To   : 세션값 가져오는 부분 어노테이션 기반으로 리팩토링 (p.200) Next : 세션 저장소로 데이터베이스 사용하기(p.200)

--- src/main/java/io/web/book/springboot/web/IndexController.java
+++ src/main/java/io/web/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package io.web.book.springboot.web;
 
+import io.web.book.springboot.config.auth.LoginUser;
 import io.web.book.springboot.config.auth.dto.SessionUser;
 import io.web.book.springboot.domain.user.User;
 import io.web.book.springboot.service.posts.PostsService;
@@ -15,13 +16,12 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName",user.getName());
