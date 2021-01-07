4895cd67b7e9ce29f6c192d1b15679739cd1c4f0
master <sungwoony@kakao.com>
2021-01-08 05:52:39 +0900

    # 배포 스크립트 - line 15 CURRET -> CURRENT 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ CURRENT_PID=$(pgrep -fl web_service | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRET_PID" ]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
