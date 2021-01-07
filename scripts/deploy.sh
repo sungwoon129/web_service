df1a2a267a8062e176cd9318f80628b04e1d41c5
master <sungwoony@kakao.com>
2021-01-08 05:11:19 +0900

    # 배포 스크립트 - current pid 찾는 명령어 수정 #2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f "web_service")
+CURRENT_PID=$(pgrep -fl web_service | grep jar | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
